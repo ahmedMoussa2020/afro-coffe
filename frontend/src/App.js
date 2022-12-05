2114715941c020a565645bb5fc39ad5b434df62a
Ahmed <>
2022-12-06 02:58:04 +0300

    setup working enviroment

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (<div>
+    <header>
+      <a href="/">Afro Bucks</a>
+    </header>
+    <main>Products</main>
+  </div>
+  );
+}
+
+export default App;
