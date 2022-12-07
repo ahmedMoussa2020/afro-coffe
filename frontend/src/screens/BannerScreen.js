a7a9d03595e975cd138beefc31228a9e4dc9d807
Ahmed <>
2022-12-07 03:08:04 +0300

    small changes made

--- frontend/src/screens/BannerScreen.js
+++ frontend/src/screens/BannerScreen.js
@@ -1,11 +1,18 @@
-import styles from '../styles/Banner.css'
+import styles from "../styles/Banner.css";
 
 function BannerScreen() {
   return (
     <div className="banner">
       <img src="/images/bannerNew.png" alt="banner" />
-      <div class="bottom-left">Bottom Left</div>
-
+      {/* <div class="bottom-left">Bottom Left</div> */}
+      <div class="text-block">
+        <h4>Quality Coffee <br/> delivered to your Door</h4>
+        <p>
+          Everything we do is a matter of heart, <br /> body and soul. <br />{" "}
+          Our mission is to provide sustainably sourced, <br /> hand-picked,
+          quality coffee.
+        </p>
+      </div>
     </div>
   );
 }
