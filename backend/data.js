f1c883ce6ff94c53a3844c939167c53ccf314a1f
Ahmed <>
2022-12-07 03:19:53 +0300

    backend created

---
+++ backend/data.js
@@ -0,0 +1,53 @@
+const data = {
+  products: [
+    {
+      name: 'Chocolate Mint Cinamon Jinger',
+      slug: 'Chocolate Mint Cinamon Jinger',
+      category: 'Coffee',
+      image: '/images/chocolate-mint-cinamon-jinger.png', //679px X 829px
+      price: 10,
+      counInStock: 20,
+      brand: 'Afro Coffee',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High quality coffe with good smell and taste'
+    },
+    {
+      name: 'Dark Chocolate Jinger Mint',
+      slug: 'Dark Chocolate Jinger Mint',
+      category: 'Coffee',
+      image: '/images/dark-chocolate-jinger-mint.png',
+      price: 10,
+      counInStock: 20,
+      brand: 'Afro Coffee',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High quality coffe with good smell and taste'
+    },
+    {
+      name: 'Dark Roast Cinamon Jinger',
+      slug: 'Dark Roast Cinamon Jinger',
+      category: 'Coffee',
+      image: '/images/dark-roast-cinamon-jinger.png',
+      price: 10,
+      counInStock: 20,
+      brand: 'Afro Coffee',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High quality coffe with good smell and taste'
+    },    {
+      name: 'Mint Cinamon Jinger',
+      slug: 'Mint Cinamon Jinger',
+      category: 'Coffee',
+      image: '/images/mint-cinamon-jinger.png',
+      price: 10,
+      counInStock: 20,
+      brand: 'Afro Coffee',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'High quality coffe with good smell and taste'
+    },
+  ]
+}
+
+export default data
