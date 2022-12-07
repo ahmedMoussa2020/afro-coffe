afc71fbbc5f65cd10744dc9654e025ac14cd6e28
Ahmed <>
2022-12-07 04:53:57 +0300

    manage state by reducer hook

--- frontend/src/screens/BannerScreen.js
+++ frontend/src/screens/BannerScreen.js
@@ -5,7 +5,7 @@ function BannerScreen() {
     <div className="banner">
       <img src="/images/bannerNew.png" alt="banner" />
       {/* <div class="bottom-left">Bottom Left</div> */}
-      <div class="text-block">
+      <div className="text-block">
         <h4>Quality Coffee <br/> delivered to your Door</h4>
         <p>
           Everything we do is a matter of heart, <br /> body and soul. <br />{" "}
--- frontend/src/screens/ProductView.js
+++ frontend/src/screens/ProductView.js
@@ -1,24 +1,46 @@
-import { useEffect, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import { Link } from "react-router-dom";
 // import data from "../data";
 import styles from "../styles/ProductView.css";
 import axios from "axios";
+import logger from "use-reducer-logger";
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true };
+    case "FETCH_SUCCESS":
+      return { ...state, products: action.payload, loading: false };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
 function ProductView() {
+  const [{ loading, error, products }, dispatch] = useReducer(logger(reducer), {
+    products: [],
+    loading: true,
+    error: "",
+  });
 
   // starts here fetching data from backend
-  const [products, setProducts] = useState([]);
+  // const [products, setProducts] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
-      const result = await axios.get("/api/products");
-      setProducts(result.data);
+      dispatch({ type: "FETCH_REQUEST" });
+      try {
+        const result = await axios.get("/api/products");
+        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL ", payload: err.message });
+      }
+      // setProducts(result.data);
     };
     fetchData();
   }, []);
   // ends here fetching data from backend
 
-
-  
   return (
     <div>
       <div className="top-div"></div>
@@ -32,23 +54,29 @@ function ProductView() {
       {/* <h1></h1> */}
       <br />
       <div className="products">
-        {products.map((product) => (
-          <div className="product" key={product.slug}>
-            <Link to={`/product/${product.slug}`}>
-              <img src={product.image} alt={product.name} />
-            </Link>
-            <div className="product-info">
+        {loading ? (
+          <div>Loading...</div>
+        ) : error ? (
+          <div>{error}</div>
+        ) : (
+          products.map((product) => (
+            <div className="product" key={product.slug}>
               <Link to={`/product/${product.slug}`}>
-                <p>{product.name}</p>
+                <img src={product.image} alt={product.name} />
               </Link>
+              <div className="product-info">
+                <Link to={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </Link>
 
-              <p>
-                <strong>${product.price}</strong>
-              </p>
-              <button>Add To Cart</button>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add To Cart</button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
