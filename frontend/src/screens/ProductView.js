10c422524a972b41869a4f0d4032c5447b15ca20
Ahmed <>
2022-12-07 04:23:11 +0300

    fetching data from backend

--- frontend/src/screens/ProductView.js
+++ frontend/src/screens/ProductView.js
@@ -1,8 +1,24 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import data from "../data";
+// import data from "../data";
 import styles from "../styles/ProductView.css";
+import axios from "axios";
 
 function ProductView() {
+
+  // starts here fetching data from backend
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios.get("/api/products");
+      setProducts(result.data);
+    };
+    fetchData();
+  }, []);
+  // ends here fetching data from backend
+
+
+  
   return (
     <div>
       <div className="top-div"></div>
@@ -11,13 +27,12 @@ function ProductView() {
         <h2>Now Roasting</h2>
         <div className="divide-block"></div>
         <p>Featuring a bright spectrum of flavors and essences</p>
-        
       </div>
 
       {/* <h1></h1> */}
       <br />
       <div className="products">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={`/product/${product.slug}`}>
               <img src={product.image} alt={product.name} />
